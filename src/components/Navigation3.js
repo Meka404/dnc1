5adb0b7da1f6686c33bcc344be711bbc9bd888b2
Carlos Albino <92502513+Meka404@users.noreply.github.com>
2023-07-10 14:52:30 -0500

    casi2

--- src/components/Navigation3.js
+++ src/components/Navigation3.js
@@ -9,13 +9,15 @@ import "../css/Navigation3.css";
 export const Navigation3 = () => {
   return (
     <Container className="navega3">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
           <Nav className="navega33 ml-auto">
             <Nav.Link href="#home">INICIO</Nav.Link>
             <Nav.Link href="#nosotros">NOSOTROS</Nav.Link>
             <NavDropdown title="SERVICIOS" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/1">INSPECCION TELEVISIVA </NavDropdown.Item>
-              <NavDropdown.Item href="#action/2">SANEAMIENTO AMBIENTAL</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3">TOPOGRAFIA INTEGRAL</NavDropdown.Item>
+              <NavDropdown.Item href="#servicios">INSPECCION TELEVISIVA </NavDropdown.Item>
+              <NavDropdown.Item href="#servicios">SANEAMIENTO AMBIENTAL</NavDropdown.Item>
+              <NavDropdown.Item href="#servicios">TOPOGRAFIA INTEGRAL</NavDropdown.Item>
              
             </NavDropdown>
             <Nav.Link href="#proyectos">PROYECTOS</Nav.Link>
