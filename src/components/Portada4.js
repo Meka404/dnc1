a835e75b32df183b6e4a17b61c637bb46a02bd70
Carlos Albino <92502513+Meka404@users.noreply.github.com>
2023-07-11 16:29:41 -0500

    espera p

--- src/components/Portada4.js
+++ src/components/Portada4.js
@@ -1,42 +1,70 @@
 import React, { useEffect, useState } from "react";
 import "../css/Portada4.css";
-import red1 from "../assets/construccion2.jpg";
-import red2 from "../assets/balde.jpg";
-import red3 from "../assets/televisiva12.jpg";
+import red3 from "../assets/saneamiento1.jpg";
+import red2 from "../assets/topografia1.jpg";
+import red1 from "../assets/televisiva12.jpg";
 import red4 from "../assets/agua.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { Container, Row, Col } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
-
+import Card from 'react-bootstrap/Card';
 export const Portada4 = () => {
   return (
-  
-      <Carousel className="grupo">
-        <Carousel.Item interval={3000}>
-          <img className="d-block w-100" src={red1} alt="First slide" />
-          <Carousel.Caption>
-            <h3>Inspeccion televisiva</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>
-          <img className="d-block w-100" src={red2} alt="Second slide" />
-          <Carousel.Caption>
-            <h3>TOPGRAFIA</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>
-          <img className="d-block w-100" src={red3} alt="Third slide" />
-          <Carousel.Caption>
-            <h3>SANEAMIENTO AMBIENTAL</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-
+    <Carousel className="grupo">
+      <Carousel.Item interval={3000}>
+        <img className="d-block w-100" src={red1} alt="First slide" />
+        <Carousel.Caption>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>INSPECCION TELEVISIVA</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Card Subtitle
+              </Card.Subtitle>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              
+            </Card.Body>
+          </Card>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={3000}>
+        <img className="d-block w-100" src={red2} alt="Second slide" />
+        <Carousel.Caption>
+        <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>TOPOGRAFIA</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+       
+      </Card.Body>
+    </Card>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={3000}>
+        <img className="d-block w-100" src={red3} alt="Third slide" />
+        <Carousel.Caption>
+        <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>SANEAMIENTO AMBIENTAL</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        
+      </Card.Body>
+    </Card>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
